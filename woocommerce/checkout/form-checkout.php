7966387f673a08b13b8654d08cc5ae366523354c
chidang <dangtanminhchi@gmail.com>
2025-10-28 14:50:21 +0700

    Update text

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -53,7 +53,7 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
     <span class="woocommerce-input-wrapper">
       <label class="checkbox ">
 	<input type="checkbox" class="input-checkbox " name="shipping_is_local_pickup" id="shipping_is_local_pickup" value="1"> 
-  Ophalen op ons adres: Drukkersweg 14, 2031 EE Haarlem&nbsp;</label></span>
+  Een account aanmaken: Drukkersweg 14, 2031 EE Haarlem&nbsp;</label></span>
   <a href="https://www.google.com/maps/place/Drukkersweg+14,+2031+EE+Haarlem,+Netherlands/@52.3991484,4.6576888,340m/data=!3m1!1e3!4m15!1m8!3m7!1s0x47c5ef8040ed48b3:0x558491be95398e63!2sDrukkersweg+14,+2031+EE+Haarlem,+Netherlands!3b1!8m2!3d52.3989582!4d4.6594854!16s%2Fg%2F11q2ndfcr3!3m5!1s0x47c5ef8040ed48b3:0x558491be95398e63!8m2!3d52.3989582!4d4.6594854!16s%2Fg%2F11q2ndfcr3?entry=ttu&g_ep=EgoyMDI1MTAyMi4wIKXMDSoASAFQAw%3D%3D" target="_blank">Bekijk locatie op Google Maps</a>
 </p>
 	
